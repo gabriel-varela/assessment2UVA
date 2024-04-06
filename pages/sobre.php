193ef58b391b6a38ba03e86deec14680a2e7e100
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-04-06 19:00:04 -0300

    Concluída a configuração inicial do site, incluindo a estrutura básica e a navegação funcional

---
+++ pages/sobre.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="height=device-height, initial-scale=1.0">
+  <link rel="stylesheet" href="../assets/styles/index.css">
+  <link rel="shortcut icon" href="" type="image/png">
+  <script src="https://kit.fontawesome.com/6302ea1cad.js" crossorigin="anonymous"></script>
+  <title>TechSprint Solutions</title>
+</head>
+
+<body>
+  <header>
+    <div class="Navbar">
+      <div class="Navbar__Link Navbar__Link-brand">
+        <a class="logotype" href="../">
+          <img src="../assets/image/LogoWithoutBG03.svg" alt="logo">
+          TechSprint
+        </a>
+      </div>
+      <div class="Navbar__Link Navbar__Link-toggle">
+        <i class="fas fa-bars"></i>
+      </div>
+      <nav class="Navbar__Items">
+        <div class="Navbar__Link">
+          <a class="Link_Element-1" href="produtos.php">Produtos e Serviços</a>
+        </div>
+        <div class="Navbar__Link">
+          <a class="Link_Element-2" href="novidades.php">Novidades</a>
+        </div>
+        <div class="Navbar__Link">
+          <a class="Link_Element-3" href="sobre.php">Sobre</a>
+        </div>
+        <div class="Navbar__Link">
+          <a class="Link_Element-4" href="contato.php">Contato</a>
+        </div>
+      </nav>
+    </div>
+  </header>
+
+  <main>
+
+  </main>
+
+  <footer>
+    <p>&copy; 2024 TechSprint</p>
+  </footer>
+
+  <script src="../assets/js/script.js"></script>
+</body>
+
+</html>
